4d3771b70a7947f15bd85333bcc5d5e304b7a99c
Edvinas Jonauskas <jonauskas.edvinas0@gmail.com>
2025-02-13 14:31:50 +0200

    Add project files.

---
+++ PirmaPaskaita/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Iveskite jusu vardas: ");
+string vardas = Console.ReadLine();
+
+Console.WriteLine("Iveskite jusu pavarde: ");
+string pavarde = Console.ReadLine();
+
+Console.WriteLine("Jusu pilnas vardas: " + vardas + " " + pavarde);
